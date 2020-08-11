58be4fd7d99606298b98b6efb71db0f14fa6d025
dChiamp <derek.chiampas@gmail.com>
2020-08-11 13:23:50 -0700

    begins adding doomsday fonts, styles

---
+++ components/FetchTest.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="fetch-test">
+        <h2>Fetch Test</h2>
+        <ul>
+            <li
+                v-for="post of posts"
+                :key="post.id"
+            >
+                <nuxt-link :to="`/posts/${post.id}`">
+                    {{ post.title }}
+                </nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    async fetch() {
+        this.posts = await this.$http.$get(
+            "https://jsonplaceholder.typicode.com/posts"
+        )
+    },
+    data() {
+        return {
+            posts: [],
+        }
+    },
+    mounted() {
+        console.log("hello world")
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.fetch-test {
+    h2 {
+        // font-family: var(--font-secondary);
+    }
+}
+</style>
---
+++ stories/FetchTest.stories.js
@@ -0,0 +1,11 @@
+// TODO: DELETE this example after Drew sees it, DELETE FetchTest component too
+import FetchTest from "@/components/FetchTest"
+
+export default {
+    title: "@WpComponents / FetchTest",
+}
+
+export const Default = () => ({
+    components: { FetchTest },
+    template: `<fetch-test />`,
+})
